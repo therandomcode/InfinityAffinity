b593c2f1a3651a9654652d81964d1bf38d228b8c
Kris Wagner <kriswagner.ch@gmail.com>
2019-08-16 15:08:46 +0200

    Improve text procesing

--- process-text.js
+++ process-text.js
@@ -1,6 +1,13 @@
 function isMinimallyInteresting(str){
-	if (str.split(" ").length < 5){
+	if (str == "undefined" || str == ""){
+		console.log("isMinimallyInteresting says something went wrong! str does not exist.");
+		return;
+	}
+	if (str.split(" ").length < 9){
 		return false; 
+	}
+	if (str.includes("bitbounce")){
+		return false;
 	}
 	if (isUselessPraise(str)){ 
 		return false; 
@@ -8,16 +15,31 @@ function isMinimallyInteresting(str){
 	if (isObscene(str)) { 
 		return false; 
 	}
-	return true;
+	return cleanMessage(str);
 }
 
-function trimMessage(str){
+function cleanMessage(str){
+	if (str == "undefined" || str == ""){
+		console.log("cleanMessage says something went wrong! str does not exist.");
+		return;
+	}
+	str = trimMessage(str);
+	str = removeEmail(str);
+	str = removeURL(str);
+	return str; 
+}
+
+function trimMessage(userMessage){
 	/* Get rid of annoying email chains, footers, long links, 
 	automated emails and support's responses */
-	var newstr = str; 
-	var strings = [ 
+	userMessage = userMessage.toLowerCase();
+	var newMessage = userMessage; 
+	var badstrings = [ 
 		"Este email puede contener",
 		"Hung from Smallpdf",
+		"Frodo at Smallpdf",
+		"Hung at Smallpdf",
+		"Frodo from Smallpdf", 
 		"The information contained in",
 		"info@smallpdf.com",
 		"Sent from",
@@ -37,27 +59,22 @@ function trimMessage(str){
 		"intended solely for the use",
 		"legally privileged",
 		"is a registered trademark",
-		"not the intended recipient"
+		"not the intended recipient",
+		"The info contained in",
+		"tel:",
+		"mobile:",
+		"sent from my iphone"
 	];
-	for (var i = 0; i < strings.length; i++){
-		newstr = str.split(strings)[0]; 
-		if (newstr.length <= 1 || newstr == str){ 
-			str = removeEmail(str);
-			str = removeURL(str);
-			return str;
-		}
-		newstr = removeEmail(newstr);
-		newstr = removeURL(newstr);
-		return newstr; 
+
+	for (var i = 0; i < badstrings.length; i++){
+		var badstring = badstrings[i].toLowerCase();
+		newMessage = newMessage.split(badstring)[0]; // get only first part of split message
 	}
+	if (newMessage.length <= 1 || newMessage == userMessage){ 
+		return userMessage;
+	}
+	return newMessage; 
 }
-
-function deSmallpdf(str){
-    var newstr = str.replace('https://smallpdf.com/','');
-    newstr.replace('https://support.smallpdf.com/','');
-    return newstr; 
-}
-
 
 function isObscene(str){
 	/* If the feedback appears to only contain red flags, 
@@ -71,7 +88,8 @@ function isObscene(str){
 		"cunt",
 		"blowjob", 
 		"motherfucking", 
-		"fuck"
+		"fuck",
+		"none of your business"
 	]
 	str = str.toLowerCase();
 	str = str.replace(/'/g, ''); //remove punctuation bc users tend to not use
@@ -80,9 +98,6 @@ function isObscene(str){
 		str = str.replace(redflags[i], '');
 	}
 	if (str.split(" ").length < 5){
-		console.log("****************");
-		console.log(oldstring);
-		console.log(str); 
 		return true;
 	}
 	return false; 
@@ -91,7 +106,8 @@ function isObscene(str){
 function isUselessPraise(str){
 	/* If the feedback appears to only contain red flags, 
 	don't show*/
-	var oldstring = str; 
+	var newMessage = str; 
+	//var oldstring = str; 
 	var redflags = [
 		"wonderful job",
 		"no nothing",
@@ -162,25 +178,26 @@ function isUselessPraise(str){
 		"e", "and", "is", "eh", 
 		"simples", 
 		"bueno",
-		"limit"
+		"limit",
+		"per hour"
 	]
-	str = str.toLowerCase();
-	str = str.replace(/'/g, ''); //remove punctuation bc users tend to not use
-	str = str.replace ("it is", "its");
-	for (var i = 0; i < redflags.length; i++){
-		str = str.replace(redflags[i], '');
+	newMessage = newMessage.toLowerCase();
+	newMessage = newMessage.replace(/'/g, ''); //remove punctuation bc users tend to not use
+	newMessage = newMessage.replace ("it is", "its");
+	for (var badstring = 0; badstring < redflags.length; badstring++){
+		newMessage = newMessage.replace(redflags[badstring], '');
 	}
-	if (str.split(" ").length < 3){
-		console.log("****************");
-		console.log(oldstring);
-		console.log(str); 
+	newMessage = (newMessage.split(" ")).join(" "); //get rid of duplicate spaces 
+	if (newMessage.split(" ").length < 4){
 		return true;
 	}
 	return false; 
 }
 
 function cleanSmallpdfUrl(str){
-	return str.replace("https://smallpdf.com/",'');
+	str = str.replace("https://smallpdf.com/",'');
+	str = str.replace('https://support.smallpdf.com/','');
+	return str; 
 }
 
 function removeURL(str){
@@ -189,9 +206,9 @@ function removeURL(str){
 	var newstr = ""; 
 	for (var i = 0; i < words.length; i++){
 		var word = words[i];
-		if (!(word.includes("http"))){
+		if (!(word.includes("http")) && !(word.includes("www."))){
 			newstr = newstr + " " + word; 
-		}
+		} 
 	}
 	return newstr.trim(); 
 }
@@ -216,7 +233,7 @@ function isInterestingTag(str){
 	str = str.replace(/\s+/g,' '); 
 	if ( str != ""
 	&& str.replace(" ", "") != ""
-	&& str.length < 40
+	&& str.length < 30
 	&& str.length > 4
 	&& (str.indexOf('@') < 0) // string is not just an email 
 	&& (str.replace(/\d/g, "").length > 0) // string is not entirely numbers
@@ -230,4 +247,68 @@ function isInterestingTag(str){
 	} else {
 		return false; 
 	}
+}
+
+function trimTag(str){
+	if (str == null || str == ""){
+		return "";
+	}
+	if (str.length > 30){
+		str = str.split(" ").slice(0,4).join(" ");
+		str = str.trim();
+		str += "...";
+	}
+	return str; 
+}
+
+/* Test functions */
+function runTests(){
+	console.log("Testing...\n" ); 
+	var message0 = "hello, pizza, this email is confidential. Please do not redistribute the content if it wasn't intended for you.";
+	var message1 = "Hello, this is a simple user message. The information contained in this email may be confidential.";
+	var message2 = "no thank you";
+	var message3 = "Nothing, its perfect";
+	var message5 = "Hi, I use ilovepdf (https://ilovepdf.com). My email is bob@dylan.com. I don't like marshmellows.";
+	var message6 = "fuck you I hate this softare";
+	var message7 = "suck my dick"
+	var message8 = "fuck this software, but I also have some useful-ish feedback. There is something wrong in part 3. I wasted a ton of time on this. Fuck you again.";
+	var message9 = "Here is something else: [www.somethingoranother.com.br/somethingelse.jpg]";
+	var message10 = "there was something or another in a message, from john doe, assistant to jane doe [www.weufhwohfweiufh.co.in address1, address2 line, some town, USA. 089-1232-4112]";
+
+	console.log("Testing isUselessPraise()..."); 
+	if (isUselessPraise(message2) == false){
+		console.log("⚠ TEST FAILED on message2");
+	} 
+	if (isUselessPraise(message3) == false){
+		console.log("⚠ TEST FAILED on message3");
+	}
+	console.log("...isUselessPraise() passed!\n");
+	
+	console.log("Testing trimMessage()...");
+	if (trimMessage((message0)).length > 40){
+		console.log("⚠ TEST FAILED on message0");
+	}
+	if (trimMessage(message1).length > 40){
+		console.log("⚠ TEST FAILED on message1"); 
+	}
+
+	console.log("Testing removeURL()...");
+	if (removeURL(message5) != "Hi, I use ilovepdf My email is bob@dylan.com. I don't like marshmellows."){
+		console.log("⚠ TEST FAILED on message5");
+	}
+	if (removeURL(message9) != "Here is something else:"){
+		console.log("⚠ TEST FAILED on message9"); 
+	}
+
+	console.log("Testing cleanMessage()...");
+		if (cleanMessage(message10).length > 275){
+			console.log("⚠ TEST FAILED on message10");
+		}
+
+	console.log("Testing trimTag...");
+		if (trimTag(message0) != "there was something or..."){
+			console.log("⚠ TEST FAILED on message10"); 
+		}
+	
+	console.log("Tests passed!");
 }
